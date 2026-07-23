6a9c1af77ba6adce82d67036accb330fecd18bce
hondaminori <hondaminori.ta3@gmail.com>
2026-07-23 17:18:50 +0900

    20260723 微修正

--- src/Kyokasho/chap5/Mondai5_12/Test.java
+++ src/Kyokasho/chap5/Mondai5_12/Test.java
@@ -3,8 +3,8 @@ package Kyokasho.chap5.Mondai5_12;
 class Test {
     static int x; static int y;
     Test(int x, int y) {
-        this.x = x;
-        this.y = y;
+        // this.x = x;
+        // this.y = y;
     }
     void print() {
         System.out.print(x + ":" + y);
